47d2ef25da87d442263a982e9b3798a0f7d4dc38
Chris Woodby <chriswoodby@Chriss-MBP.lan>
2022-01-18 14:48:36 -0600

    validation function core functionality

--- app/Numbers.tsx
+++ app/Numbers.tsx
@@ -1,31 +1,36 @@
 import React from "react";
 
 export default function Numbers(props:any) {
-    function validate(board){
+    const validate = function(){
         var grid = [];
         // first horizonal rows
-        for (var i = 0; i < board.length; i++) {
-          if (board[i].indexOf('0') !== -1) {
-            return 'Try again!'
-          }
-          var unique = [...new Set(board[i])];
-          if (board[i].length !== unique.length) {
-            return 'Try again!';
+        for (var i = 0; i < props.grid.length; i++) {
+          var tracker = {};
+          for (var j = 0; j < props.grid[i].length; j++) {
+            if (!tracker[props.grid[i][j]] && Number(props.grid[i][j])) {
+              tracker[props.grid[i][j]] = true;
+            } else if (tracker[props.grid[i][j]]){
+              alert('Try again!');
+            }
           }
         }
         // second columns
         for (var i = 0; i < 9; i++) {
           var inner = []
-          for (var j = 0; j < board.length; j++) {
-            inner.push([board[j][i]]);
+          for (var j = 0; j < props.grid.length; j++) {
+            inner.push([props.grid[j][i]]);
           }
           grid.push(inner);
         }
         // check columns
           for (var i = 0; i < grid.length; i++) {
-          var unique = [...new Set(grid[i])];
-          if (grid[i].length !== unique.length) {
-            return 'Try again!';
+           var tracker = {};
+          for (var j = 0; j < grid[i].length; j++) {
+            if (!tracker[grid[i][j]] && Number(grid[i][j])) {
+              tracker[grid[i][j]] = 1;
+            } else if (tracker[grid[i][j]]) {
+              alert('try again')
+            }
           }
         }
         // last put each 3X3 cube into an array
@@ -37,12 +42,12 @@ export default function Numbers(props:any) {
         var cube = [];
         var index = 0;
         while (index !== 9) {
-          for (var i = bigstart; i < board.length - bigend; i++) {  
-            for (var j = start; j < board[i].length - end; j++) {
-              cube.push(board[i][j]);
+          for (var i = bigstart; i < props.grid.length - bigend; i++) {  
+            for (var j = start; j < props.grid[i].length - end; j++) {
+              cube.push(props.grid[i][j]);
             }
           }
-          cubeContainer.push(cube)
+          cubeContainer.push(cube);
           index += 1
           if (start === 0 && end === 6) {
             start = 3;
@@ -63,9 +68,18 @@ export default function Numbers(props:any) {
           }
           cube = [];
         }
-        console.log(cubeContainer);
-        return 'Finished';
+        for (var i = 0; i < cubeContainer.length; i++) {
+          var cubeTracker = {};
+          for (var j = 0; j < cubeContainer[i].length; j++) {
+            if (!cubeTracker[cubeContainer[i][j]] && Number(cubeContainer[i][j])) {
+              cubeTracker[cubeContainer[i][j]] = true;
+            } else if (cubeTracker[cubeContainer[i][j]]) {
+              alert('try again')
+            }
+          }
+        }
       }
+
 
 
   const placement = function(num, i:number, j:number) {
@@ -74,7 +88,7 @@ export default function Numbers(props:any) {
     }
     var board = props.grid
     board[i][j] = num;
-    props.setGrid(board);
+    props.setGrid(board); 
     validate();
   }
 
